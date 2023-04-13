f21d3f55188f4be113d7fee4f341145457082df6
Rajesh Bethu <rajeshbethu25@gmail.com>
2023-04-13 22:57:40 +0530

    todo about async storage

--- src/components/GenerateRandNum.js
+++ src/components/GenerateRandNum.js
@@ -30,7 +30,8 @@ export default function GenerateRandNum() {
 let tablesArray = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25,
-];
+]; // Now work on generating this Tables Array based on user's selection
+//Store the table values in Async Storage in selection screen then fetch this array from there
 let lastRandomTable = null;
 export function GenerateRandTable() {
   let newBeginnning = false;
